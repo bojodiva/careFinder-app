13c0834b38dfe23551afefd9c1836fb0f95bc735
Ojo Abisola <104736729+bojodiva@users.noreply.github.com>
2023-08-04 13:25:53 +0100

    Create LoadingSpinner.jsx

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function LoadingSpinner(){
+  return(
+    <>
+      <div className="loading--spinner">
+        <span className="spinner--dot"></span>
+        <span className="spinner--dot"></span>
+        <span className="spinner--dot"></span>
+        
+      </div>
+    </>
+  )
+}
+
