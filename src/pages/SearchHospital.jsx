d97386e7bc0bda0d4bbfb624043ed91272d3cbdf
Ojo Abisola <104736729+bojodiva@users.noreply.github.com>
2023-08-04 21:14:02 +0100

    Create SearchHospital.jsx

---
+++ src/pages/SearchHospital.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { ref, onValue } from 'firebase/database';
+import {database} from "./firebase"
+
+export default function SearchHospitals({ onSearch }) {
+  const [hospitals, setHospitals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    
+    const hospitalsRef = ref(database, "hospitals");
+
+    const unsubscribe = onValue(hospitalsRef, (snapshot) => {
+      const data = snapshot.val();
+     
+        const hospitalsArray = data ? Object.values(data) : [];
+      setHospitals(hospitalsArray);
+      setIsLoading(false); 
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleSearch = () => {
+    const filteredHospitals = hospitals.filter((hospital) =>
+      hospital.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    onSearch(filteredHospitals); 
+      setSearchTerm("")
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+    </>
+  );
+}
