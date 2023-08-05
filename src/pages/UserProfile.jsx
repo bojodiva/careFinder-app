fb5490f12fad9b14f3a70ec679a80ba5be72ca83
Ojo Abisola <104736729+bojodiva@users.noreply.github.com>
2023-08-05 23:12:54 +0100

    Update UserProfile.jsx

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import {Outlet, NavLink} from "react-router-dom"
+// import { ref, uploadBytes, getDownloadURL } from "firebase/storage"
+import picture from "./images/carefinder-logo.png";
+import picture1 from "./images/avatar--profile.png";
+
+
+export default function UserProfile() {
+  return (
+    <>
+      <div>
+        <div className='profile--nav'>
+  <div className='nav--logo'>
+     <img src={picture} className="logo"></img>
+      <h2 className='logo--name'>CareFinder</h2>
+      </div>
+          
+            <div className="image--container">  
+                <img src={picture1}
+        className="avatar" 
+        alt="Clickable Image"
+        style={{ cursor: 'pointer' }}>
+      </img>
+ </div>
+</div>
+        <div className="profile--container">
+        <div className="profile--links">
+          <div>
+          <NavLink to="/profile/user" className="profile--link">Profile</NavLink>
+          <NavLink to="/profile/add-hospital" className="profile--link">Add Hospital</NavLink>
+            </div>
+        </div>
+          <div>
+             <Outlet/>
+            </div>
+          </div>
+        </div>
+      </>
+  );
+}
+
+
+
+
